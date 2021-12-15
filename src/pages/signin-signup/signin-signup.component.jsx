d6d3fd360dab2bfffe6a46b4354102ce0e9707e0
JonArryn <dumpster@lordofthevale.com>
2021-12-14 20:12:32 -0800

    add signup page and sign in form components

---
+++ src/pages/signin-signup/signin-signup.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./signin-signup.styles.scss";
+import SignIn from "../../components/sign-in/sign-in.component";
+
+const SignInSignUpPage = () => (
+  <div className="signin-signup">
+    <h1>Sign In</h1>
+    <SignIn />
+  </div>
+);
+
+export default SignInSignUpPage;
